5127b3012fec9e4bc09622b3ab0a0fe6ae0292eb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-01-15 17:01:00 +0700

    Add arraydata: Sample::DeNiro

---
+++ lib/ArrayData/Sample/DeNiro.pm
@@ -0,0 +1,118 @@
+package ArrayData::Sample::DeNiro;
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Role::Tiny::With;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+with 'ArrayDataRole::Source::LinesInDATA';
+
+1;
+# ABSTRACT: List of Robert De Niro's movies, in chronological order
+
+__DATA__
+Greetings
+Bloody Mama
+Hi,Mom!
+Born to Win
+Mean Streets
+Bang the Drum Slowly
+The Godfather,Part II
+The Last Tycoon
+Taxi Driver
+1900
+New York,New York
+The Deer Hunter
+Raging Bull
+True Confessions
+The King of Comedy
+Once Upon a Time in America
+Falling in Love
+Brazil
+The Mission
+Dear America: Letters Home From Vietnam
+The Untouchables
+Angel Heart
+Midnight Run
+Jacknife
+We're No Angels
+Awakenings
+Stanley & Iris
+Goodfellas
+Cape Fear
+Mistress
+Guilty by Suspicion
+Backdraft
+Thunderheart
+Night and the City
+This Boy's Life
+Mad Dog and Glory
+A Bronx Tale
+Mary Shelley's Frankenstein
+Casino
+Heat
+Sleepers
+The Fan
+Marvin's Room
+Wag the Dog
+Jackie Brown
+Cop Land
+Ronin
+Great Expectations
+Analyze This
+Flawless
+The Adventures of Rocky & Bullwinkle
+Meet the Parents
+Men of Honor
+The Score
+15 Minutes
+City by the Sea
+Analyze That
+Godsend
+Shark Tale
+Meet the Fockers
+The Bridge of San Luis Rey
+Rent
+Hide and Seek
+The Good Shepherd
+Arthur and the Invisibles
+Captain Shakespeare
+Righteous Kill
+What Just Happened?
+Everybody's Fine
+Machete
+Little Fockers
+Stone
+Killer Elite
+New Year's Eve
+Limitless
+Silver Linings Playbook
+Being Flynn
+Red Lights
+Last Vegas
+The Big Wedding
+Grudge Match
+Killing Season
+The Bag Man
+Joy
+Heist
+The Intern
+Dirty Grandpa
+Hands of Stone
+The Comedian
+The Wizard of Lies
+Joker
+The Irishman
+The War with Grandpa
+The Comeback Trail
+Amsterdam
+Savage Salvation
+Killers of the Flower Moon
+About My Father
+Ezra
